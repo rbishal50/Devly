ac2a884d8c909bc5b9c4e1947b76312cf844e566
Bishal Rana <rbishal50@gmail.com>
2025-06-10 19:47:52 +0300

    Add and use next-themes and fix issues

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 
+import ThemeProvider from "@/context/Theme";
+
 import "./globals.css";
 
 const inter = localFont({
@@ -29,11 +31,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${inter.className} ${spaceGrotesk.variable} antialiased`}
       >
-        {children}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
