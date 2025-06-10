6183e30fc0f9ac6a5b19f6f0f38d481c7eac4541
Bishal Rana <rbishal50@gmail.com>
2025-06-10 13:44:53 +0300

    Add custom fonts

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,8 @@
 export default function Home() {
-  return <h1 className="text3xl">Welcome</h1>;
+  return (
+    <>
+      <h1 className="h1-bold">Welcome</h1>
+      <h1 className="h1-bold font-space-grotesk">Welcome</h1>
+    </>
+  );
 }
