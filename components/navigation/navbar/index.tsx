ff63b02d663522a0046f05df8756ed4b2931aab1
Bishal Rana <rbishal50@gmail.com>
2025-06-10 20:13:15 +0300

    Add a navbar and theme toggler

---
+++ components/navigation/navbar/index.tsx
@@ -0,0 +1,31 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+import ThemeToggler from "./ThemeToggler";
+
+const Navbar = () => {
+  return (
+    <nav className="flex-between background-light900_dark200 fixed z-50 w-full gap-5 p-6 shadow-light-300 dark:shadow-none sm:px-12">
+      <Link href="/" className="flex items-center gap-1">
+        <Image
+          src="/images/site-logo.svg"
+          width={23}
+          height={23}
+          alt="Devly Logo"
+        />
+
+        <p className="h2-bold font-space-grotesk text-dark-100 dark:text-light-900 max-sm:hidden">
+          Devly<span className="text-primary-500">Pro</span>
+        </p>
+      </Link>
+      {/* TODO */}
+      {/* <p>Global Search</p> */}
+      <div className="flex-between gap-5">
+        <ThemeToggler />
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
