78bc65a6eb8345565af3fe20f0fc3b30a2d6abb8
Bishal Rana <rbishal50@gmail.com>
2025-06-11 16:30:12 +0300

    Add a auth form for traditional sign/in/up

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,7 +1,19 @@
+"use client";
+
 import React from "react";
 
-const SignUP = () => {
-  return <div>Sign up</div>;
+import AuthForm from "@/components/forms/AuthForm";
+import { SignUpSchema } from "@/lib/validations";
+
+const SignUp = () => {
+  return (
+    <AuthForm
+      formType="SIGN_UP"
+      schema={SignUpSchema}
+      defaultValues={{ email: "", password: "", name: "", username: "" }}
+      onSubmit={(data) => Promise.resolve({ success: true, data })}
+    />
+  );
 };
 
-export default SignUP;
+export default SignUp;
