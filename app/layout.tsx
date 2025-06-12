74925974a2ef2622a5f28298d89ac0da54a76e09
Bishal Rana <rbishal50@gmail.com>
2025-06-12 13:43:20 +0300

    Update site description

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,8 +22,7 @@ const spaceGrotesk = localFont({
 
 export const metadata: Metadata = {
   title: "Devly",
-  description:
-    "A community-driven platform for asking and answering programming questions. Get help, share knowledge, and collaborate with developers from around the world. Explore topics in web development, mobile app development, algorithms, data structures, and more.",
+  description: "A platform to share and expand your knowledge!",
   icons: {
     icon: "/images/site-logo.svg",
   },
