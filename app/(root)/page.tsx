43e07daff4caea4746b9ebb69cc38144b02bc7cf
Bishal Rana <rbishal50@gmail.com>
2025-06-23 12:27:30 +0300

    Implement logout and show user on nav

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,13 +1,10 @@
 import Link from "next/link";
 
-import { auth } from "@/auth";
 import QuestionCard from "@/components/cards/QuestionCard";
 import HomeFilter from "@/components/filters/HomeFilter";
 import LocalSearch from "@/components/search/LocalSearch";
 import { Button } from "@/components/ui/button";
 import ROUTES from "@/constants/routes";
-import { api } from "@/lib/api";
-import handleError from "@/lib/handlers/error";
 
 const questions: Question[] = [
   {
@@ -66,10 +63,6 @@ const Home = async ({ searchParams }: SearchParams) => {
       : true;
     return matchesQuery && matchesFilter;
   });
-
-  const session = await auth();
-
-  console.log({ session });
 
   return (
     <>
