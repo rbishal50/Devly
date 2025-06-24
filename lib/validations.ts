24bed5433c659ec43186e126ec53c8bebdef2542
Bishal Rana <rbishal50@gmail.com>
2025-06-24 11:31:13 +0300

    Implement edit question

--- lib/validations.ts
+++ lib/validations.ts
@@ -68,6 +68,10 @@ export const AskQuestionSchema = z.object({
     .max(3, { message: "Cannot add more than 3 tags." }),
 });
 
+export const EditQuestionSchema = AskQuestionSchema.extend({
+  questionId: z.string().min(1, { message: "Question id is required!" }),
+});
+
 export const UserSchema = z.object({
   name: z.string().min(1, { message: "Name is required." }),
   username: z
